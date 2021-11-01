cd21045db1fff95ed914ba0837ec0076e42c1093
gitgitWi <wiii@kakao.com>
2021-11-02 00:02:53 +0900

    Feat (Basics) add Composables

--- week 1-Jetpack Compose basics/MyCodeLab/app/src/main/java/dev/tutorials/compose_basics/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyCodeLab/app/src/main/java/dev/tutorials/compose_basics/MainActivity.kt
@@ -3,36 +3,54 @@ package dev.tutorials.compose_basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import dev.tutorials.compose_basics.ui.theme.JetpackComposeBasicsTheme
+import androidx.compose.ui.unit.dp
+import dev.tutorials.compose_basics.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // instead of using XML, call Composable funcs
         setContent {
-            JetpackComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            BasicsCodelabTheme {
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun MyApp(color: Color = MaterialTheme.colors.background) {
+    // 배경색만 사용자 정의하고, 텍스트색은 검-흰 중 MaterialTheme에서 알아서 제공
+    Surface(color = color) {
+        Greeting("gitgitWi")
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackComposeBasicsTheme {
-        Greeting("Android")
+    BasicsCodelabTheme {
+        MyApp()
+    }
+}
+
+@Preview(showBackground = true, name = "second default preview")
+@Composable
+fun DefaultPreview2() {
+    BasicsCodelabTheme {
+        MyApp(MaterialTheme.colors.secondary)
     }
 }

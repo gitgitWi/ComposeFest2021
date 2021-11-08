3cabf18cea99a32155391ae473b06c64e628f5df
gitgitWi <wiii@kakao.com>
2021-11-08 23:55:55 +0900

    Feat (Basics) add a component with hoisted variable

--- week 1-Jetpack Compose basics/MyCodeLab/app/src/main/java/dev/tutorials/compose_basics/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyCodeLab/app/src/main/java/dev/tutorials/compose_basics/MainActivity.kt
@@ -3,19 +3,14 @@ package dev.tutorials.compose_basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.tutorials.compose_basics.ui.theme.BasicsCodelabTheme
@@ -26,21 +21,31 @@ class MainActivity : ComponentActivity() {
         // instead of using XML, call Composable funcs
         setContent {
             BasicsCodelabTheme {
-                val names = listOf("World", "Space")
-                Column {
-                    for (name in names)
-                        MyApp(name)
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-private fun MyApp(name: String, color: Color = MaterialTheme.colors.primary) {
-    // 배경색만 사용자 정의하고, 텍스트색은 검-흰 중 MaterialTheme에서 알아서 제공
-    Surface(color = color) {
-        Greeting(name)
+fun MyApp() {
+
+    // hoisted variable, `by` for delegating
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+
+    if (shouldShowOnboarding) {
+        // callback 함수 전달
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = listOf("World", "Space")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) Greeting(name = name)
     }
 }
 
@@ -49,6 +54,8 @@ private fun Greeting(name: String) {
     // remember를 사용해 mutable state를 기억, recompositions 방지
     val isExpanded = remember { mutableStateOf(false) }
     val extraPadding = if (isExpanded.value) 48.dp else 0.dp
+
+
 
     Row(
         modifier = Modifier
@@ -69,11 +76,31 @@ private fun Greeting(name: String) {
     }
 }
 
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                onClick = onContinueClicked,
+                modifier = Modifier.padding(vertical = 24.dp)
+            ) {
+                Text(text = "Continue")
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
-        MyApp("gitgitWi")
+        MyApp()
     }
 }
 
@@ -89,3 +116,12 @@ fun ColumnPreview() {
         }
     }
 }
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
